fab98f0892c1e2b2f6115cbd7d4010dfbf610cf4
Mateo lizcano <mateodevcode@gmail.com>
2025-05-20 21:35:08 +0200

    Se corrige el error de NuestrosServicios.tsx

--- src/components/servicios/NuestrosServicios.tsx
+++ src/components/servicios/NuestrosServicios.tsx
@@ -1,13 +1,36 @@
 "use client";
 
-import { servicios } from "@/data/servicios";
+import { Servicio } from "@/data/servicios";
+import { supabase } from "@/lib/supabaseClient";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BiPlus } from "react-icons/bi";
+import { IoMdCut } from "react-icons/io";
+import Loading from "../loading/Loading";
 
 const NuestrosServicios = () => {
   const router = useRouter();
+  const [servicios, setServicios] = useState<Servicio[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchServicios = async () => {
+      const { data, error } = await supabase
+        .from("servicios")
+        .select("*")
+        .order("created_at", { ascending: true });
+      if (error) console.error("Error:", error);
+      else setServicios(data);
+      setLoading(false);
+    };
+    fetchServicios();
+  }, []);
+
+  if (loading) {
+    return <Loading />;
+  }
+
   return (
     <div className="w-full flex items-center justify-center mt-8">
       <div className="w-10/12">
@@ -17,21 +40,34 @@ const NuestrosServicios = () => {
         <div className="flex flex-row items-center space-x-2 overflow-auto w-full justify-start py-5">
           {servicios.map((servicio, index) => (
             <div
-              className="bg-white flex flex-col items-center justify-center min-w-[150px] h-36 rounded-xl shadow-lg space-y-1.5 p-4 cursor-pointer hover:bg-zinc-200 transition duration-300"
+              className={`relative bg-white flex flex-col items-center justify-center min-w-[150px] h-36 rounded-xl shadow-lg space-y-1.5 p-4 cursor-pointer hover:bg-zinc-200 transition duration-300`}
               key={index}
+              id={String(servicio.id)}
               onClick={() => {
-                router.push("/reservar-cita?servicio=" + servicio.nombre);
+                router.push("/reservar-cita" + `?servicio=${servicio.id}`);
               }}
             >
               <div className="flex items-center justify-center space-x-1">
-                {servicio.icon && (servicio.icon as React.ReactNode)}
-                {servicio.imagen && servicio.icon && (
-                  <BiPlus className="text-xl" />
+                {servicio.nombre === "Corte de cabello" && (
+                  <IoMdCut className="text-2xl" />
                 )}
-                {servicio.imagen && (
+                {servicio.nombre === "Corte y Barba" && (
+                  <>
+                    <IoMdCut className="text-2xl" />
+                    <BiPlus className="text-xl" />
+                    <Image
+                      src={"/icon/bigote.png"}
+                      alt={"Barba"}
+                      width={100}
+                      height={100}
+                      className="w-10"
+                    />
+                  </>
+                )}
+                {servicio.nombre === "Barba" && (
                   <Image
-                    src={servicio.imagen}
-                    alt={servicio.nombre}
+                    src={"/icon/bigote.png"}
+                    alt={"Barba"}
                     width={100}
                     height={100}
                     className="w-10"
@@ -41,7 +77,12 @@ const NuestrosServicios = () => {
               <h4 className="font-semibold text-sm text-center leading-4.5">
                 {servicio.nombre}
               </h4>
-              <p className="text-base text-zinc-700">$ {servicio.precio}</p>
+              <span className="text-base text-zinc-700">
+                $ {servicio.precio}
+              </span>
+              <span className="text-black text-xs font-semibold absolute bottom-2 opacity-20">
+                Aprox. ({servicio.duracion} min)
+              </span>
             </div>
           ))}
         </div>
